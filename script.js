213f68631b195ee020337aeac6dd7064bfa9d812
Adam Gordon <adambaumgordon@gmail.com>
2021-05-27 14:48:05 -0600

    update

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-let counter = 0;
-
-// const spaceFactory = (index, value) => {
-//     return {index, value};
-// };
 
 
 const gameBoard = (() => {
+    let counter;
     const _board = [];
+
     const _update = function() {
-        console.log(this.id);
+        let newValue;
+        if (counter%2 === 0) {
+             newValue = "x";
+        } else {
+            newValue = "o";
+        }
+        _board[this.id] = newValue;
+        this.textContent = newValue.toUpperCase();
+        counter++;
+        printBoard();
     }
+
     const init = () => {
+        counter = 0;
         const wrapper = document.querySelector(".board-wrapper");
         for (let i = 0; i < 9; i++) {
-            _board[i] = "-";
+            _board[i] = "";
             const space = document.createElement("div");
             space.id = i;
             space.addEventListener("click",_update);
             space.textContent = _board[i];
+            space.classList.add("space");
             wrapper.appendChild(space);
         }
     }
@@ -30,8 +39,3 @@ const gameBoard = (() => {
 })();
 
 gameBoard.init();
-
-
-// function update () {
-//     console.log(this.id);
-// }
